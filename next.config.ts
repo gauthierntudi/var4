4bd0ea5cd0d4c01ad1a4501f7f21f179f22b2899
gauthierntudi <gauthierntudi@gmail.com>
2026-05-30 01:09:32 +0100

    ajust version Next

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(__dirname),
+  serverExternalPackages: ["@prisma/client", "prisma"],
+  outputFileTracingIncludes: {
+    "/api/inscriptions": ["./node_modules/.prisma/client/**/*"],
+  },
   allowedDevOrigins: [
     "http://192.168.1.71:3000",
     "http://localhost:3000",
